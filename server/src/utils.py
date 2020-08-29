fb592240474956a4b27a26990b604431e10afff1
Mengyang <mengyang_liu@outlook.com>
2020-08-30 00:55:18 +0800

    Remove old color picker

--- server/src/utils.py
+++ server/src/utils.py
@@ -3,18 +3,8 @@ import os
 import hashlib
 
 from expandLogger import Logger
-def color_generator(num):
-    return '0' + str(hex(num)).replace('0x', '') if num < 15 else str(hex(num)).replace('0x', '') 
-
-def random_color_generator():
-    while 1:
-        r = random.randint(128, 255)
-        g = random.randint(128, 255)
-        b = random.randint(128, 255)
-        yield '#' + color_generator(r) + color_generator(g) + color_generator(b)
 
 GLOBAL_LOGGER = Logger()
-COLOR_PICKER = random_color_generator()
 
 def generate_color_config(name, entities):
     # TODO: Define entities and color config source
