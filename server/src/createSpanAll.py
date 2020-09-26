2a0c8ce767f6e01a5ba5916d9f0b50bba873dd0b
Mengyang <mengyang_liu@outlook.com>
2020-09-26 11:54:05 +0800

    Fix createSpanAllText with annotation file gen

--- server/src/createSpanAll.py
+++ server/src/createSpanAll.py
@@ -6,20 +6,18 @@ from os.path import split as path_split
 from jsonwrap import dumps as json_dumps
 from jsonwrap import loads as json_loads
 from annotation import TEXT_FILE_SUFFIX,JOINED_ANN_FILE_SUFF
-from utils import get_entity_index_exist, get_entity_index, add_common_info
+from utils import get_entity_index_exist, get_entity_index, add_common_info, annotation_file_generate,parse_annotation_file
 
 def create_span_all_text(**kwargs):
     label = kwargs['label']
     collection = kwargs['collection']
     document = kwargs['document']
     keyword = kwargs['keyword']
-    # collection, document, keyword, label
     directory = collection
     real_dir = real_directory(directory)
     document = path_join(real_dir, document)
     txt_file_path = document + '.' + TEXT_FILE_SUFFIX
     ann_file_path = txt_file_path[:-3] + 'ann'
-    # ann_file_path = JOINED_ANN_FILE_SUFF + '.' + JOINED_ANN_FILE_SUFF
     return _create_span_all_text(txt_file_path, keyword, label, ann_file_path)
 
 
@@ -28,18 +26,27 @@ def _create_span_all_text(txt_file_path, keyword, label, ann_file_path, entity_i
     with open(txt_file_path, 'r') as txt_file:
         text = txt_file.read()
     with open(ann_file_path, 'r') as ann_file:
-        ann = ann_file.readlines()
-    for line in ann:
-        entity_index = line.split(" ")
-        entity_index = entity_index[0][1:] 
+        ann = ann_file.read()
+
+    exist_index = ann.split('\n').__len__()
     
-    entity_index = get_entity_index()
+    entity_index = get_entity_index_exist(exist_index)
 
     entities = [
         ["T" + str(next(entity_index)), label, [(pos.start(), pos.end())]]
         for pos in re.finditer(keyword, text)
     ]
     res["entities"] = entities
+    annotation_file_generate(res, ann_file_path, text, 'a')
+    cur_anns = parse_annotation_file(ann_file_path)
+    cur_entities = []
+    for cur_ann in cur_anns:
+        try:
+            if cur_ann:
+                cur_entities.append([cur_ann.id, cur_ann.type, cur_ann.spans])
+        except AttributeError:
+            pass
+    res['entities'] = cur_entities
     res = add_common_info(text, res)
     return res
 
--- server/src/labelFunctionExecutor.py
+++ server/src/labelFunctionExecutor.py
@@ -14,25 +14,7 @@ import os
 
 from utils import GLOBAL_LOGGER
 from utils import get_entity_index, clean_cached_config, add_common_info
-
-
-def annotation_file_generate(res, file_path, text):
-    anno_content = ""
-    for entity in res["entities"]:
-        anno_content += (
-            str(entity[0])
-            + "\t"
-            + str(entity[1])
-            + " "
-            + str(entity[2][0][0])
-            + " "
-            + str(entity[2][0][1])
-            + "\t"
-            + str(text[entity[2][0][0]: entity[2][0][1]])
-            + "\n"
-        )
-    with open(file_path, 'w') as f:
-        f.write(anno_content)
+from utils import annotation_file_generate
 
 def resort_entities(entity_list, func_name_list):
     all_entities = dict()
