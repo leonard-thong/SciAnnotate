712d914fa337e8d6fa1c2a0465a9f14348ca9a5d
hluo76 <robin1997@gatech.edu>
2020-10-09 22:48:54 -0400

    prehanle data

--- server/src/utils.py
+++ server/src/utils.py
@@ -106,12 +106,12 @@ def prehandle_data(**kwargs):
 def _prehandle_data(txt_file_path, ann_file_path, function_ann_file_path):
     out = []
     sentence = dict()
-    annotation = dict()
-    sentence['sentence value'] = []
-    annotation['annotation value'] = []
+    sentence['sentence'] = ''
+    sentence['annotation'] = []
     with open(ann_file_path, 'r') as ann_file:
         for line in ann_file.readlines():
             data = []
+            line = line.replace('\t', ' ')
             info = line.split(' ')
             source_name = info[1].split('_')[0]
             temp = info[1].split('_')[1:]
@@ -120,29 +120,28 @@ def _prehandle_data(txt_file_path, ann_file_path, function_ann_file_path):
                 label += temp[i]
             data.append(source_name)
             data.append(label)
-            start = info[2]
-            end = info[3]
+            start = int(info[2])
+            end = int(info[3])
             line_dict = judge_line(txt_file_path)
             line_start_index = [key for key in line_dict]
             line_start_index = sorted(line_start_index)
             for i in range(len(line_start_index)):
-                if start > line_start_index[i] and end < (line_start_index[i] + len(line_dict[line_start_index[i]])):
-                    sentence['sentence value'].append(line_dict[line_start_index[i]])
-                    start -= line_start_index[i]
-                    end -= line_start_index[i]
+                if start > int(line_start_index[i]) and end < (int(line_start_index[i]) + len(line_dict[line_start_index[i]])):
+                    sentence['sentence'] = line_dict[line_start_index[i]]
+                    start -= int(line_start_index[i])
+                    end -= int(line_start_index[i])
                     break
                 '''
                 elif start > line_start_index[i] and end > (line_start_index[i] + len(line_dict[line_start_index[i]])):
                 '''
             data.append(start)
             data.append(end)
-            annotation['annotation value'].append(data)
-            
-            
+            sentence['annotation'].append(data)     
     
     with open(function_ann_file_path, 'r') as function_ann_file:
-        for line in ann_file.readlines():
+        for line in function_ann_file.readlines():
             data = []
+            line = line.replace('\t', ' ')
             info = line.split(' ')
             source_name = info[1].split('_')[0]
             temp = info[1].split('_')[1:]
@@ -151,26 +150,24 @@ def _prehandle_data(txt_file_path, ann_file_path, function_ann_file_path):
                 label += temp[i]
             data.append(source_name)
             data.append(label)
-            start = info[2]
-            end = info[3]
+            start = int(info[2])
+            end = int(info[3])
             line_dict = judge_line(txt_file_path)
             line_start_index = [key for key in line_dict]
             line_start_index = sorted(line_start_index)
             for i in range(len(line_start_index)):
-                if start > line_start_index[i] and end < (line_start_index[i] + len(line_dict[line_start_index[i]])):
-                    sentence['sentence value'].append(line_dict[line_start_index[i]])
-                    start -= line_start_index[i]
-                    end -= line_start_index[i]
+                if start > int(line_start_index[i]) and end < (int(line_start_index[i]) + len(line_dict[line_start_index[i]])):
+                    sentence['sentence'] = line_dict[line_start_index[i]]
+                    start -= int(line_start_index[i])
+                    end -= int(line_start_index[i])
                     break
                 '''
                 elif start > line_start_index[i] and end > (line_start_index[i] + len(line_dict[line_start_index[i]])):
                 '''
             data.append(start)
             data.append(end)
-            annotation['annotation value'].append(data)
-            
+            sentence['annotation'].append(data)           
     out.append(sentence)
-    out.append(annotation)
     return out
 
 
