1b6c5c1919abe69248350be9bff9ba11b0a6013c
hluo76 <robin1997@gatech.edu>
2020-12-18 11:34:15 -0500

    update

--- server/src/deleteDocument.py
+++ server/src/deleteDocument.py
@@ -1,6 +1,13 @@
+import os
+from document import real_directory
+from os.path import join as path_join
+
 def delete_new_document(**kwargs):
     res = dict()
     collection = kwargs['collection']
     document = kwargs['document']
-    
+    directory = collection
+    real_dir = real_directory(directory)
+    document = path_join(real_dir, document)
+    os.remove(document)
     return res
