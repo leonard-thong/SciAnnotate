4ac100a81e5e201d57f279dd57267971cc91f5d1
NikolaLiu <NikolaLiu@icloud.com>
2021-03-07 00:07:33 +0800

    restore sentence offset to null

--- server/src/document.py
+++ server/src/document.py
@@ -916,6 +916,7 @@ def get_document(collection, document):
     real_dir = real_directory(directory)
     doc_path = path_join(real_dir, document)
     ori_response = _document_json_dict(doc_path)
+    del ori_response['sentence_offsets']
     return ori_response
 
 def get_document_with_sentence_offsets(collection, document):
